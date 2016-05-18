0d4732cec0723fdbb52298d2294482e7892742ee
Michael W. Delaney <michael.delaney@gmail.com>
2016-05-18 11:39:30 -0400

    Update walker_includes.php

--- walker_includes.php
+++ walker_includes.php
@@ -9,17 +9,20 @@
  *
  * @link https://github.com/roots/sage/pull/1042
  */
+ 
+  define( 'WALKER_THEME_SETUP_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
 $wss_includes = [
-  'lib/walker-theme-setup/lib/walker_setup.php',              // WSS specific setup
-  'lib/walker-theme-setup/lib/walker_branding.php',           // Brand with The Walker Group logos
-  'lib/walker-theme-setup/lib/wp_bootstrap_navwalker.php', // Bootstrap Navwalker
-  'lib/walker-theme-setup/lib/metaboxes.php',              // Register and manipulate metaboxes
-  'lib/walker-theme-setup/lib/custom_admin.php'            // Add custom scripts and styles to admin
+  'lib/walker_setup.php',              // WSS specific setup
+  'lib/walker_branding.php',           // Brand with The Walker Group logos
+  'lib/wp_bootstrap_navwalker.php', // Bootstrap Navwalker
+  'lib/metaboxes.php',              // Register and manipulate metaboxes
+  'lib/custom_admin.php'            // Add custom scripts and styles to admin
 
 ];
 
 foreach ($wss_includes as $file) {
-  if (!$filepath = locate_template($file)) {
+  if ( !$filepath = WALKER_THEME_SETUP_PLUGIN_DIR . $file ) {
     trigger_error(sprintf(__('Error locating %s for inclusion', 'sage'), $file), E_USER_ERROR);
   }
 
