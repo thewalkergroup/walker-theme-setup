6c83758d6a073eeec69797048f5d6b3df6a8443e
Michael W. Delaney <michael.delaney@gmail.com>
2016-06-16 15:00:15 -0400

    Update walker_includes.php

--- walker_includes.php
+++ walker_includes.php
@@ -12,11 +12,12 @@ License: MIT
 define( 'WALKER_THEME_SETUP_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
 $wss_includes = [
-  'lib/walker_setup.php',              // WSS specific setup
-  'lib/walker_branding.php',           // Brand with The Walker Group logos
-  'lib/wp_bootstrap_navwalker.php', // Bootstrap Navwalker
-  'lib/metaboxes.php',              // Register and manipulate metaboxes
-  'lib/custom_admin.php'            // Add custom scripts and styles to admin
+  'lib/walker_setup.php',             // WSS specific setup
+  'lib/walker_branding.php',          // Brand with The Walker Group logos
+  'lib/wp_bootstrap_navwalker.php',   // Bootstrap Navwalker
+  'lib/wp_bootstrap_pagination.php',  // Bootstrap Pagination
+  'lib/metaboxes.php',                // Register and manipulate metaboxes
+  'lib/custom_admin.php'              // Add custom scripts and styles to admin
 
 ];
 
