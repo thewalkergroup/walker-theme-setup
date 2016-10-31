3bced43d4079686282f63d6c4b035e4f9fae4e0d
Michael W. Delaney <michael.delaney@gmail.com>
2016-10-31 09:11:11 -0400

    Update walker_setup.php

--- lib/walker_setup.php
+++ lib/walker_setup.php
@@ -1,13 +1,4 @@
 <?php
 namespace Roots\Sage\Setup;
 
-use Roots\Sage\Assets;
-
-/**
- * Theme setup
- */
-function wss_setup() {
-  //add_theme_support( 'custom-logo' );
-
-}
-add_action('after_setup_theme', __NAMESPACE__ . '\\wss_setup');
+add_filter('acf/settings/show_admin', '__return_false');
