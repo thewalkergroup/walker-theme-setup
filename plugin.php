def144d43860c718cb600229884803b672799b22
Michael W. Delaney <michael.delaney@gmail.com>
2017-07-07 13:12:54 -0400

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -19,14 +19,10 @@ require __DIR__ . '/vendor/autoload.php';
 define( 'WALKER_THEME_SETUP_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'WALKER_THEME_SETUP_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
-use Branding;
 $wss_branding = new \WSS\Setup\Branding();
 
-use Init;
 $wss_metaboxes = new \WSS\Setup\Init();
 
-use ACF;
 $wss_acf = new \WSS\Setup\ACF();
 
-use Metaboxes;
 $wss_metaboxes = new \WSS\Setup\Metaboxes();
