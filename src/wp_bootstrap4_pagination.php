eafe186ab8e8d0faa2cd9258e1fbe66825edbda6
Michael W. Delaney <michael.delaney@gmail.com>
2017-07-07 13:25:49 -0400

    Update wp_bootstrap4_pagination.php

--- src/wp_bootstrap4_pagination.php
+++ src/wp_bootstrap4_pagination.php
@@ -3,7 +3,7 @@
  * WordPress Bootstrap 4 Pagination
 */
 function wp_bootstrap4_pagination( $args = array() ) {
-    
+
     $defaults = array(
         'range'           => 4,
         'custom_query'    => FALSE,
@@ -12,25 +12,25 @@ function wp_bootstrap4_pagination( $args = array() ) {
         'before_output'   => '<nav aria-label="Page Navigation"><ul class="pagination">',
         'after_output'    => '</ul></nav>'
     );
-    
-    $args = wp_parse_args( 
-        $args, 
+
+    $args = wp_parse_args(
+        $args,
         apply_filters( 'wp_bootstrap_pagination_defaults', $defaults )
     );
-    
+
     $args['range'] = (int) $args['range'] - 1;
     if ( !$args['custom_query'] )
         $args['custom_query'] = @$GLOBALS['wp_query'];
     $count = (int) $args['custom_query']->max_num_pages;
     $page  = intval( get_query_var( 'paged' ) );
     $ceil  = ceil( $args['range'] / 2 );
-    
+
     if ( $count <= 1 )
         return FALSE;
-    
+
     if ( !$page )
         $page = 1;
-    
+
     if ( $count > $args['range'] ) {
         if ( $page <= $args['range'] ) {
             $min = 1;
@@ -46,11 +46,11 @@ function wp_bootstrap4_pagination( $args = array() ) {
         $min = 1;
         $max = $count;
     }
-    
+
     $echo = '';
     $previous = intval($page) - 1;
     $previous = esc_attr( get_pagenum_link($previous) );
-    
+
     $firstpage = esc_attr( get_pagenum_link(1) );
     if ( $firstpage && (1 != $page) )
         $echo .= '<li class="page-item"><a class="page-link" href="' . $firstpage . '">' . __( 'First', 'text-domain' ) . '</a></li>';
@@ -65,17 +65,17 @@ function wp_bootstrap4_pagination( $args = array() ) {
             if ($page == $i) {
                 $echo .= '<li class="page-item active"><a class="page-link">' . str_pad( (int)$i, 2, '0', STR_PAD_LEFT ) . '</a></li>';
             } else {
-                $echo .= sprintf( '<li class="page-item><a class="page-link" href="%s">%002d</a></li>', esc_attr( get_pagenum_link($i) ), $i );
+                $echo .= sprintf( '<li class="page-item"><a class="page-link" href="%s">%002d</a></li>', esc_attr( get_pagenum_link($i) ), $i );
             }
         }
     }
-    
+
     $next = intval($page) + 1;
     $next = esc_attr( get_pagenum_link($next) );
     if ($next && ($count != $page) )
-        $echo .= '<li class="page-item><a class="page-link" href="' . $next . '" title="' . __( 'next', 'text-domain') . '">' . $args['next_string'] . '</a></li>';
+        $echo .= '<li class="page-item"><a class="page-link" href="' . $next . '" title="' . __( 'next', 'text-domain') . '">' . $args['next_string'] . '</a></li>';
     else
-        $echo .= '<li class="page-item disabled"><a class="page-link" href="#" title="' . __( 'next', 'text-domain') . '">' . $args['next_string'] . '</a></li>';        
+        $echo .= '<li class="page-item disabled"><a class="page-link" href="#" title="' . __( 'next', 'text-domain') . '">' . $args['next_string'] . '</a></li>';
     $lastpage = esc_attr( get_pagenum_link($count) );
     if ( $lastpage ) {
         $echo .= '<li class="page-item next"><a class="page-link" href="' . $lastpage . '">' . __( 'Last', 'text-domain' ) . '</a></li>';
